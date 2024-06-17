11a01c616149f66618d193bb423244fed8001a5f
晋 <1540175452@qq.com>
2024-06-17 13:37:36 +0800

    增加Facade.php 增加DebugTime.php

---
+++ src/Facade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Xin\Support;
+
+abstract class Facade
+{
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     */
+    public static function __callStatic(string $name, array $arguments)
+    {
+        return call_user_func_array([static::getFacadeAccessor(), $name], $arguments);
+    }
+
+    /**
+     * @return mixed
+     */
+    abstract public static function getFacadeAccessor();
+}
