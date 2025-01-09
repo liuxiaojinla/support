ccce87bd7a55ecf25d4111724492f941ae14d596
晋 <1540175452@qq.com>
2025-01-09 11:57:31 +0800

    升级优化

--- src/Callback.php
+++ src/Callback.php
@@ -12,8 +12,20 @@ final class Callback
 	 */
 	public static function safeCall(callable $callback, callable $failedCallback = null)
 	{
+		return self::safeCallWithArgs($callback, [], $failedCallback);
+	}
+
+	/**
+	 * 安全调用Callback，增加参数传递
+	 * @param callable $callback
+	 * @param array $args
+	 * @param callable|null $failedCallback
+	 * @return mixed
+	 */
+	public static function safeCallWithArgs(callable $callback, array $args = [], callable $failedCallback = null)
+	{
 		try {
-			return call_user_func($callback);
+			return call_user_func_array($callback, $args);
 		} catch (\Throwable $e) {
 			$failedCallback && call_user_func($failedCallback, $e);
 		}
